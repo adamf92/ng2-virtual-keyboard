94f4f2533f6a3b00b1a4f847252d083c34400cfc
Adam Filpek <adamfilipek1@gmail.com>
2018-10-15 21:27:23 +0200

    Public api exports - vk-component and directives

--- projects/ng2-virtual-keyboard/src/public_api.ts
+++ projects/ng2-virtual-keyboard/src/public_api.ts
@@ -1,3 +1,4 @@
+
 /*
  * Public API Surface of ng2-virtual-keyboard
  */
@@ -5,3 +6,7 @@ export * from './lib/services/virtual-keyboard.service';
 export * from './lib/models/key-event.model';
 export { Ng2VkCustomKeyboardModel } from './lib/models/qwerty-keyboard.model';
 export * from './lib/ng2-virtual-keyboard.module';
+export { AfVkInputDirective as Ng2VkInput } from './lib/directives/input.directive';
+export { AfVkTextAreaDirective as Ng2VkTextArea } from './lib/directives/text-area.directive';
+export { AfVkToggleDirective as Ng2VkToggle } from './lib/directives/toggle.directive';
+export { AfVirtualKeyboardComponent as Ng2VirtualKeyboard } from './lib/components/virtual-keyboard.component';
